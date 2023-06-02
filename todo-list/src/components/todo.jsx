682b4bf88c978b0a7ecaae5ea8e39a873fb62f9b
Christophertechn <njengachris42@gmail.com>
2023-06-02 12:15:14 +0300

    todo

---
+++ todo-list/src/components/todo.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// const Todo = (props) => {
+//     const { todo } = props
+
+
+
+//     return (
+
+//         <div className= 'todo'>
+//         <span>Wash dishes</span>
+//         <div className='actions'>
+//         <i className='fas fa-check'></i>
+//         <i className='fas fa-trash'></i>
+//         <i className='fas fa-loading'></i>
+
+//         </div>
+//         </div>
+
+//     )
+// }
---
+++ todo-list/src/components/todos.jsx
@@ -0,0 +1,15 @@
+// import  React from 'react'
+
+// import Todo from './todo'
+
+// const Todos =() =>{
+//     return (
+//         <div className='todos'>
+//             <h4>Todo-List</h4>
+//             <Todo/>
+//         </div>
+//     )
+// }
+
+
+// export default Todo
